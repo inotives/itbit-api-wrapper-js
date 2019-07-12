3cb239b52bd9d3ce8e20e9406da7b4209aae048e
ToniLim <tlim@itbit.com>
2019-07-12 16:11:33 +0800

    amend README and re-org some examples

--- README.md
+++ README.md
@@ -10,5 +10,37 @@ This wrapper is written as example for connecting itBit Exchange API using javas
 
 - Clone this repo
 - CD into the project root and run `npm install`
-- After installation, run `node _sample.js` by default it will run the public endpoint of Itbit API, Private endpoint will return signature error unless API cred is added.
-- Edit the _sample.js file with your api credential to test private endpoint
+- After installation, create a `creds.json` file with the following format:
+
+```
+[
+	{
+		"key": "your-itbit-key",
+		"secret": "your-itbit-secret",
+		"userId": "your-itbit-userid"
+	}
+]
+```
+
+- run the example code with this: `npm run example`
+
+
+### List of Endpoints wrappers
+Here are all the endpoints wrappers listed on [itBit REST-API documentation site](https://api.itbit.com/docs).
+
+
+#### Public Endpoint:
+
+- **getTickers(market)**: get the tickers by market
+- **getOrderbooks(market)**: get the orderbooks by market
+
+#### Private Endpoints:
+
+- **getWallets()**: get all user wallets
+- **getWallet(walletId)**: get a wallet infos by its walletId
+- **newWallet(walletName)**: create a new wallet
+- **getWalletTrades(walletId, query)**: get all the trades executed on that wallet.
+- **newOrder(walletId, side, type, curr, amt, price, market, metadata, coi)**: create new order
+- **getOrders(walletId, query)**: list orders in a wallet
+- **getOrder(walletId, orderId)**: get an order details by its orderId
+- **cancelOrder(walletId, orderId)**: cancel an open id
--- _sample.js
+++ _sample.js
@@ -3,71 +3,26 @@ const creds = require('./creds.json');
 
 const api = new ItbitApi(creds[0].key, creds[0].secret, creds[0].userId)
 
-// PUBLIC ENDPOINT EXAMPLES:
+const examples = async () => {
 
-// calling GET TICKER
-api.getTickers("XBTUSD")
-  .then( res => {
-    console.log("XBTUSD-TICKER")
-    console.log(res.data)
-  })
+  const ticker = await api.getTickers("XBTUSD")
+    .then( res => res.data )
 
-// calling GET ORDER BOOKS
-// api.getOrderbooks("XBTUSD")
-//   .then( orderbook => {
-//       console.log(orderbook.data)
-//   })
-
-// PRIVATE ENDPOINT EXAMPLES:
-
-
-// caling GET ALL WALLETS
-api.getWallets()
-  .then( wallets => {
-    console.log("WALLETS:")
-    console.log(wallets.data)
-  })
-
-// Creating New Wallet
-// api.newWallet("XXTest")
-//   .then( resp => {
-//     console.log(resp.data)
-//   })
+  const wallets = await api.getWallets()
+    .then( wallets => wallets.data )
 
 
 
-// caling GET A WALLET
-// api.getWallet("b903d575-47ad-4972-80d1-c49a66192cf5")
-//   .then( wallets => {
-//     console.log(wallets.data)
-//   })
+  console.log("----------------------\nPUBLIC ENDPOINT:\n----------------------")
+  console.log(ticker)
 
-// List all the trades history in a wallet
-// api.getWalletTrades("b903d575-47ad-4972-80d1-c49a66192cf5")
-//   .then( trades => {
-//     console.log(trades.data)
-//   })
+  console.log("----------------------\nPRIVATE ENDPOINT:\n----------------------")
+  console.log(wallets)
 
-// create new order
-// api.newOrder("d576984c-7463-41ad-bbdb-5cb3336e5eab", 'buy', 'limit', 'XBT', 0.00001, 5000, 'XBTUSD')
-//   .then( order => {
-//     console.log(order.data)
-//   })
+  console.log("List User Orders ....")
+  const orders = await api.getOrders( wallets[0].id, {status: 'filled'})
+    .then( orders => orders.data)
+  console.log(orders)
+}
 
-// // list orders
-// api.getOrders("d576984c-7463-41ad-bbdb-5cb3336e5eab", {status: 'open'})
-//   .then( orders => {
-//     console.log(orders.data)
-//   })
-//
-// get 1 order
-// api.getOrder("d576984c-7463-41ad-bbdb-5cb3336e5eab", "f0dafc84-e3f0-4378-afbf-7d3136a4227e")
-//   .then( order => {
-//     console.log(order.data)
-//   })
-
-// // cancelling order
-// api.cancelOrders("d576984c-7463-41ad-bbdb-5cb3336e5eab", "f0dafc84-e3f0-4378-afbf-7d3136a4227e")
-//   .then( order => {
-//     console.log(order.data)
-//   })
+examples();
